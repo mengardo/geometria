6b1af1e4be1fb62fbb4786d1f8e29b0fdee99dee
Raul Rodriguez <tortosaraul@gmail.com>
2022-04-28 07:22:10 +0200

    Version refactorizada

--- geometria/src/Circulo_RRT.java
+++ geometria/src/Circulo_RRT.java
@@ -1,10 +1,10 @@
 
-public class Circulo extends FiguraGeometrica {
+public class Circulo_RRT extends FiguraGeometrica_RRT {
 	private double radio;
 	//Constante pi
 	static final double PI = 3.1416;
 	
-	public Circulo(double r, String tipoFigura) {
+	public Circulo_RRT(double r, String tipoFigura) {
 		super(tipoFigura);
 		radio = r;
 	}
--- geometria/src/FiguraGeometrica_RRT.java
+++ geometria/src/FiguraGeometrica_RRT.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_RRT {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_RRT(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
